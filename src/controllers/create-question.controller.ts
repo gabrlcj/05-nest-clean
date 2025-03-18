d7ba6b8bb7dfe54fdadfbaa4b47f249f8a55249f
gabrlcj <gabrielbittencourt57@gmail.com>
2025-03-18 18:32:39 -0300

    feat: adds custom decorator to get user info from jwt

--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
+import { CurrentUser } from 'src/auth/current-user-decorator'
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard'
+import { UserPayload } from 'src/auth/jwt.strategy'
 import { PrismaService } from 'src/prisma/prisma.service'
 
 @Controller('questions')
@@ -8,7 +10,9 @@ export class CreateQuestionController {
 
   @Post()
   @UseGuards(JwtAuthGuard)
-  async handle() {
+  async handle(@CurrentUser() user: UserPayload) {
+    console.log(user)
+
     return 'ok'
   }
 }
